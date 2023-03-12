4b3a3c8aece92c89d8ca573b000966023db19d0a
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-03-12 14:44:38 -0400

    Create entity for Registrations data

--- backend/entities.py
+++ backend/entities.py
@@ -5,6 +5,7 @@ from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 from typing import Self
 from models import User
+from models import Registration
 
 
 class Base(DeclarativeBase):
@@ -19,7 +20,43 @@ class Base(DeclarativeBase):
 # TODO: Create an entity for Roles data (Ajay)
 
 # TODO: Create an entity for Registrations data (Jade)
+class RegistrationEntity(Base):
+    """Serves as the database model schema defining the shape of the `Registrations` table."""
+    
+    __tablename__ = "registrations"
 
+    # Unique ID for a registration
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    # User ID associated with registration
+    user_id: Mapped[int] = mapped_column(Integer)
+    # Event ID associated with registration
+    event_id: Mapped[int] = mapped_column(Integer)
+    # Status of Registration (0 = Registered, 1 = Registered + Attended)
+    status: Mapped[int] = mapped_column(Integer)
+
+    @classmethod
+    def from_model(cls, model: Registration) -> Self:
+        """
+        Class method that converts a `Registration` object into a `RegistrationEntity`
+        
+        Parameters:
+            - model (Registration): Model to convert into an entity
+        
+        Returns:
+            RegistrationEntity: Entity created from model
+        """
+        return cls(id=model.id, user_id=model.user_id, event_id=model.event_id, status=model.status)
+
+    def to_model(self) -> User:
+        """
+        Class method that converts a `RegistrationEntity` into a `Registration` object
+        
+        Parameters: None
+        
+        Returns:
+            Registration: `Registration` object from the entity
+        """
+        return Registration(id=self.id, user_id=self.user_id, event_id=self.event_id, status=self.status)
 
 
 """ This is a sample entity """
