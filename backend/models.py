59a8cf49146f542528b09066a4222fa84a83190c
Ajay Gandecha <ajgandecha@gmail.com>
2023-03-11 21:16:08 -0500

    Set up Repository for Group Work
    This comment removes all functionality from ex04 while keeping the skeleton for the start of the project. I also included # TODO tags for each task.

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
 
-class User(BaseModel):
-    pid: int 
-    first_name: str
-    last_name: str
+""" Sample models """
+
+# class User(BaseModel):
+#     pid: int 
+#     first_name: str
+#     last_name: str
