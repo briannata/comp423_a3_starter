74f623a597235abdecafc0a2a275aa7cbc1f3449
Ajay Gandecha <ajgandecha@gmail.com>
2023-03-11 21:23:16 -0500

    Add extra comments

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,15 @@
 from pydantic import BaseModel
 
+# TODO: Create database models for User data (Audrey)
+
+# TODO: Create database models for Organization data (Brianna)
+
+# TODO: Create database models for Events data (Brianna)
+
+# TODO: Create database models for Roles data (Ajay)
+
+# TODO: Create database models for Registrations data (Jade)
+
 """ Sample models """
 
 # class User(BaseModel):
