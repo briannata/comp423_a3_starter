94cd8dcf913908cd73e4c1ebb2544e7a72270853
jadekeegan <97476936+jadekeegan@users.noreply.github.com>
2023-03-12 21:07:32 -0400

    Add implementation to create method in registrations service

---
+++ backend/registrations_service.py
@@ -0,0 +1,60 @@
+from fastapi import Depends
+from sqlalchemy import select
+from sqlalchemy import insert
+from sqlalchemy.orm import Session
+from database import db_session
+from models import Registration, User, Event
+from entities import RegistrationEntity
+
+""" This is a sample service """
+
+class RegistrationsService:
+
+    _session: Session
+
+    def __init__(self, session: Session = Depends(db_session)):
+        self._session = session
+
+    def all(self) -> list[Registration]:
+        query = select(RegistrationEntity)
+        entities = self._session.scalars(query).all()
+        return [entity.to_model() for entity in entities]
+
+    def create(self, user: User, event: Event) -> Registration:
+      # Attempt to get RegistrationEntity based on the IDs of the given user and event.
+      registration = self._session.query(RegistrationEntity).filter(
+        RegistrationEntity.user_id == user.id, 
+        RegistrationEntity.event_id == event.id)
+
+      # Check if the registration already exists in the table.
+      if registration:
+        # If the registration exists, raise a value error with a description.
+        raise ValueError(f"{user.first_name} {user.last_name} is already registered for the event '{event.name}'.")
+      else:
+        # If the registration does not exist, create a new registration.
+        registration_entity = RegistrationEntity.from_model()
+        
+
+    # def create(self, registration: Registration) -> Registration:
+    #     if self._session.get(RegistrationEntity, registration.id):
+    #         raise ValueError(f"Duplicate user found with PID: {user.pid}")
+    #     else:
+    #         user_entity = UserEntity.from_model(user)
+    #         self._session.add(user_entity)
+    #         self._session.commit()
+    #         return user_entity.to_model()
+
+    # def get(self, pid: int) -> User | None:
+    #     user = self._session.get(UserEntity, pid)
+    #     if user:
+    #         return user.to_model()
+    #     else:
+    #         raise Exception(f"No user found with PID: {pid}")
+
+    # def delete(self, pid: int) -> None:
+    #     obj=self._session.query(UserEntity).filter(UserEntity.pid==pid).first()
+    #     if obj:
+    #         self._session.delete(obj)
+    #         self._session.commit()
+    #     else:
+    #         raise Exception(f"No user found with PID: {pid}")
